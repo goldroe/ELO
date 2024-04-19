b3a93c4b8e5ed02d48b2180be194cf602f92c6c1
Carlos Rivera <cr4185320@gmail.com>
2024-04-18 22:10:07 -0500

    Update elo.cpp

--- src/elo.cpp
+++ src/elo.cpp
@@ -6,9 +6,8 @@
 #include "lexer.h"
 #include "parser.h"
 #include "ast_dump.h"
+#include "semantic.h"
 
-Atom *make_atom(const char *str, size_t len);
-Atom *make_atom(const char *str);
 int main(int argc, char **argv) {
     argc--; argv++;
     if (argc == 0) {
@@ -18,20 +17,37 @@ int main(int argc, char **argv) {
     }
     
     char *file_name = argv[0];
-    
-    init_keywords();
-    init_atom_map();
 
-    {
+    bool do_ast_dump = false;
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-astdump") == 0) {
+            do_ast_dump = true;
+        }
+    }
+    
+    init_atom_map();
+    init_keywords();
+
+#if 0
         assert(make_atom("foo") == make_atom("foo"));
         assert(make_atom("buzz") == make_atom("buzz"));
         assert(make_atom("Entity") == make_atom("Entity"));
         assert(make_atom("obj") != make_atom("Obj"));
-    }
+        assert(make_atom("foo") == make_atom("foo"));
+#endif
     
     Parser parser(file_name);
 
-    Ast_Dump ast_dump;
-    ast_dump.dump(parser.root);
+
+    Sema_Analyzer sema_analyzer;
+    sema_analyzer.init(&parser);
+    sema_analyzer.resolve();
+
+    if (do_ast_dump) {
+        printf("\n");
+        Ast_Dump ast_dump;
+        ast_dump.dump(parser.root);
+    }
+
     return 0;
 }
