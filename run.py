e154a704cefbb6a37a61938ab6d766e1147cc071
Carlos Rivera <cr4185320@gmail.com>
2025-05-09 16:22:52 -0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+import sys
+
+file_name = sys.argv[1]
+byte_file = os.path.splitext(file_name)[0] + '.bc'
+
+process = subprocess.run(['elo', file_name], capture_output=True, text=True)
+print(process.stdout, end='')
+print(process.stderr, end='')
+
+# Check the return code
+if process.returncode == 0:
+    process = subprocess.run(['llvm-dis', byte_file], capture_output=True, text=True)
+    print(process.stdout, end='')
+    print(process.stderr, end='')
+    if process.returncode == 0:
+        process = subprocess.run(['clang', os.path.splitext(file_name)[0] + '.ll'], capture_output=True, text=True)
+        print(process.stdout, end='')
+        print(process.stderr, end='')
