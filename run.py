e451f75b6eeeec024590eefbf438d95cc4048eb2
Carlos Rivera <cr4185320@gmail.com>
2025-05-30 15:20:00 -0500

    out filename0

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 file_name = sys.argv[1]
-byte_file = os.path.splitext(file_name)[0] + '.bc'
+# byte_file = os.path.splitext(file_name)[0] + '.bc'
 
 process = subprocess.run(['elo', file_name], capture_output=True, text=True)
 print(process.stdout, end='')
@@ -11,10 +11,10 @@ print(process.stderr, end='')
 
 # Check the return code
 if process.returncode == 0:
-    process = subprocess.run(['llvm-dis', byte_file], capture_output=True, text=True)
+    process = subprocess.run(['llvm-dis', 'out.bc'], capture_output=True, text=True)
     print(process.stdout, end='')
     print(process.stderr, end='')
     if process.returncode == 0:
-        process = subprocess.run(['clang', os.path.splitext(file_name)[0] + '.ll'], capture_output=True, text=True)
+        process = subprocess.run(['clang', 'out.ll'], capture_output=True, text=True)
         print(process.stdout, end='')
         print(process.stderr, end='')
